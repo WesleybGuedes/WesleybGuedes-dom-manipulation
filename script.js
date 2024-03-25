72c5b243e490962f447eaf9985b7d899fd068945
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-03-25 20:25:36 -0300

    feat: Exercise 1, 2 and 3

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Seu código aqui
+// Exercise: 1
+const changeParagraph = () => {
+  const paragraph = document.getElementById('paragraph');
+  paragraph.innerHTML = 'Working like a boss!';
+};
+
+changeParagraph();
+
+// Exercise: 2
+const changeColor = () => {
+  const colorContent = document.getElementsByClassName('main-content');
+  colorContent[0].style.backgroundColor = 'rgb(76,164,109)';
+};
+
+changeColor();
+
+// Exercise: 3
+const changeSecondColor = () => {
+  const colorContent = document.getElementsByClassName('center-content');
+  colorContent[0].style.backgroundColor = 'white';
+};
+
+changeSecondColor();
