54102835a4d07feca36af8e11d78f3c6b42214c8
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-03-25 21:56:06 -0300

    feat: Exercise 4, 5 and 6

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ const changeSecondColor = () => {
 };
 
 changeSecondColor();
+
+// Exercise: 4
+
+const changeTitle = () => {
+  const title = document.getElementsByTagName('h1');
+  title[0].innerHTML = 'Desafio - JavaScript';
+};
+
+changeTitle();
+
+// Exercise: 5
+const uperCase = () => {
+  const paragraph = document.getElementsByTagName('p');
+  paragraph[0].innerHTML = paragraph[0].innerHTML.toUpperCase();
+};
+
+uperCase();
+
+// Exercise: 6
+const showAllParagraph = () => {
+  const allParagraphs = document.getElementsByTagName('p');
+  let paragraph = '';
+  for (let i = 0; i < allParagraphs.length - 1; i += 1) {
+    paragraph += `${allParagraphs[i].innerHTML} `;
+  }
+  allParagraphs[3].innerHTML = paragraph;
+};
+
+showAllParagraph();
